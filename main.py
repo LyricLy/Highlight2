8520c10e9eeac45a0e858aba2cce559d351b2a70
LyricLy <gulliverhanson@gmail.com>
2021-09-12 01:32:16 +1200

    Fix issue with sending history

--- main.py
+++ main.py
@@ -103,8 +103,8 @@ def english_list(l, merger="and"):
         return f"{', '.join(l[:-1])}, {merger} {l[-1]}"
 
 async def send_highlight(user, patterns, msg):
-    before = await msg.channel.history(before=msg, limit=2, oldest_first=True).flatten()
-    after = await msg.channel.history(after=msg, limit=2, oldest_first=True).flatten()
+    before = (await msg.channel.history(before=msg, limit=2).flatten())[::-1]
+    after = await msg.channel.history(after=msg, limit=2).flatten()
 
     lines = []
     for message in before + [None] + after:
